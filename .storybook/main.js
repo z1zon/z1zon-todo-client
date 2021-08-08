eaa7acac29a88b72b63bbc661e0bb29916b49ec1
oh.jihoon <oh.jihoon@linecorp.com>
2021-08-08 16:19:02 +0900

    Chore : add addon-jest plugin

--- .storybook/main.js
+++ .storybook/main.js
@@ -18,6 +18,7 @@ module.exports = {
     '@storybook/addon-controls',
     '@storybook/addon-knobs',
     '@storybook/addon-actions',
+    '@storybook/addon-jest',
   ],
   typescript: {
     check: false,
